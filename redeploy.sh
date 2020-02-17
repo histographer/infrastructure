ec9bb5703985319c51f7432c3fc0d46c56eeb2c4
Tor Berre <tor0405@hotmail.no>
2020-02-17 16:18:58 +0100

    fix: removed unused line in redeploy.sh

--- redeploy.sh
+++ redeploy.sh
@@ -2,6 +2,5 @@
 
 # Use this to redeploy services through a CI/CD setup
 # usage: ./redeploy.sh <name-of-docker-service>
-cd ../cytomine/bootstrap
 docker-compose build --pull --no-cache $1
 docker-compose up -d $1
