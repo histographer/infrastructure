888fc61b3915f63ffa95a48b9fb383f3fd97e922
Tor Berre <tor0405@hotmail.no>
2020-03-05 12:47:15 +0100

    fix: update redeploy script

--- redeploy.sh
+++ redeploy.sh
@@ -2,6 +2,4 @@
 
 # Use this to redeploy services through a CI/CD setup
 # usage: ./redeploy.sh <name-of-docker-service>
-cd /home/cytomine/bootstrap
-docker-compose build --pull --no-cache $1
-docker-compose up -d $1
+docker service update $1
