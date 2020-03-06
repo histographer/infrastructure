dcbdfe2aabf8216845a8da8157b1e757ef4fef70
Tor Berre <tor0405@hotmail.no>
2020-03-06 11:15:44 +0100

    fix: nginx healthcheck

--- nginx/run.sh
+++ nginx/run.sh
@@ -23,4 +23,30 @@ function secretsToEnv() {
 
 configure /tmp/nginx.conf.sample /usr/local/nginx/conf/nginx.conf
 
-/tmp/deploy.sh
+mkdir -p /tmp/uploaded && chmod 777 /tmp/uploaded
+
+echo "/usr/local/nginx/logs/access.log {"   > /etc/logrotate.d/nginx
+echo "  copytruncate"                      >> /etc/logrotate.d/nginx
+echo "  daily"                             >> /etc/logrotate.d/nginx
+echo "  size 250M"                         >> /etc/logrotate.d/nginx
+echo "  rotate 14"                         >> /etc/logrotate.d/nginx
+echo "  compress"                          >> /etc/logrotate.d/nginx
+echo "  missingok"                         >> /etc/logrotate.d/nginx
+echo "  create 640 root root"              >> /etc/logrotate.d/nginx
+echo "}"                                   >> /etc/logrotate.d/nginx
+echo ""                                    >> /etc/logrotate.d/nginx
+echo "/usr/local/nginx/logs/error.log {"   >> /etc/logrotate.d/nginx
+echo "  copytruncate"                      >> /etc/logrotate.d/nginx
+echo "  daily"                             >> /etc/logrotate.d/nginx
+echo "  size 250M"                         >> /etc/logrotate.d/nginx
+echo "  rotate 14"                         >> /etc/logrotate.d/nginx
+echo "  compress"                          >> /etc/logrotate.d/nginx
+echo "  missingok"                         >> /etc/logrotate.d/nginx
+echo "  create 640 root root"              >> /etc/logrotate.d/nginx
+echo "}"                                   >> /etc/logrotate.d/nginx
+
+
+echo "Launch of nginx"
+/usr/local/nginx/sbin/nginx || { echo 'Nginx crashed!' ; exit 1; }
+
+tail -F /usr/local/nginx/logs/access.log
