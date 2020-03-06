86b79442740b5c630161372d8c27c74417e362b6
Tor Berre <tor0405@hotmail.no>
2020-03-06 11:47:31 +0100

    fix: update redeploy script

--- redeploy.sh
+++ redeploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # Use this to redeploy services through a CI/CD setup
-# usage: ./redeploy.sh <name-of-docker-service>
-docker service update $1
+# usage: ./redeploy.sh <image> <name-of-docker-service>
+docker service update --image $1 $2
