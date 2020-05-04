1f43cb13798e11882db081c16d1a47538eb78fbd
Kent <kentaretorvik@gmail.com>
2020-05-04 11:12:36 +0200

    feat: add down bash file

---
+++ docker-containers-down.sh
@@ -0,0 +1,4 @@
+docker-compose -f docker-compose.traefik.yml down
+docker-compose -f docker-compose.cytomine.yml down
+docker-compose -f docker-compose.wizard.yml down
+docker-compose -f docker-compose.compare.yml down
--- docker-containers-up.sh
+++ docker-containers-up.sh
@@ -1,4 +1,8 @@
 docker network create traefik
+docker-compose -f docker-compose.traefik.yml pull
+docker-compose -f docker-compose.cytomine.yml pull
+docker-compose -f docker-compose.wizard.yml pull
+docker-compose -f docker-compose.compare.yml pull
 docker-compose -f docker-compose.traefik.yml up -d
 docker-compose -f docker-compose.cytomine.yml up -d
 docker-compose -f docker-compose.wizard.yml up -d
