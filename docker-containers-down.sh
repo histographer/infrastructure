030d04cd650e1997eb3fcbed5545e4e75f7e2c98
Kent <kentaretorvik@gmail.com>
2020-05-04 11:25:59 +0200

    fix: forgot analysis in bash file

--- docker-containers-down.sh
+++ docker-containers-down.sh
@@ -2,3 +2,4 @@ docker-compose -f docker-compose.traefik.yml down
 docker-compose -f docker-compose.cytomine.yml down
 docker-compose -f docker-compose.wizard.yml down
 docker-compose -f docker-compose.compare.yml down
+docker-compose -f docker-compose.analysis.yml down
